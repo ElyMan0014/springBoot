bf41cf88804a637ef05b65daf75c391979863c9c
eliasmanai <eliasmanai@192.168.2.130>
2019-04-23 23:16:36 +0200

    first fix on the controller class

--- springBoot/springboot/src/main/java/springmain/Controller/StudentController.java
+++ springBoot/springboot/src/main/java/springmain/Controller/StudentController.java
@@ -3,6 +3,7 @@ package springmain.Controller;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ public class StudentController {
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Student getStudentById(int id) {
+	public Student getStudentById(@PathVariable("id") int id) {
 		return studentService.getStudentById(id);
 	}
 }
