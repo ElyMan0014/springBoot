daf155c62712f82d641c12d468270afa8f6575d1
eliasmanai <eliasmanai@192.168.2.130>
2019-04-23 22:44:30 +0200

    first commit on main method

--- springBoot/springboot/src/main/java/springmain/Main.java
+++ springBoot/springboot/src/main/java/springmain/Main.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
 
+	// the main method where it will run the spring application.
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 
